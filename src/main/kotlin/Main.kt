e8072afd97763ad303e5fd1b6d8214e328aafed9
Anna Frolova <an.frolova023@gmail.com>
2024-12-24 01:19:53 +0700

    KTB-02 add file for storing vocabulary

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,11 @@
+import java.io.File
+
+
 fun main() {
 
+    val wordsFile = File("words.txt")
+    val wordsList = wordsFile.readLines()
+    for (word in wordsList) {
+        println(word)
+    }
 }
