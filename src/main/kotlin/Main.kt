96a5f59b8adc5c9d577b150b12e2a20fac2b029d
Anna Frolova <an.frolova023@gmail.com>
2024-12-25 17:26:16 +0700

    KTB-02 refactor: replace loop with forEachLine to read lines

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,7 @@
 import java.io.File
 
-
 fun main() {
 
     val wordsFile = File("words.txt")
-    val wordsList = wordsFile.readLines()
-    for (word in wordsList) {
-        println(word)
-    }
+    wordsFile.forEachLine { println(it) }
 }
